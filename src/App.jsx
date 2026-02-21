1da9f2e9744fea46fe65895440728a3a4c7b0bb1
Riddhima Sharma <riddhimaaa24@gmail.com>
2026-02-21 23:02:28 +0530

    Initial React+Tailwind Setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <button className="bg-red-600 text-white text-3xl px-16 py-16 rounded-full shadow-lg hover:bg-red-700">SOS</button>
+    </div>
+  )
+}
+
+export default App
